8f07bd0bb56fc30a8814a37b73cfbeb21baf262b
Clement Escoffier <clement.escoffier@gmail.com>
2015-11-05 17:50:32 +0100

    Implementation of the recommendation service

--- recommendation-service/src/main/java/io/vertx/workshop/recommendation/impl/RecommendationServiceImpl.java
+++ recommendation-service/src/main/java/io/vertx/workshop/recommendation/impl/RecommendationServiceImpl.java
@@ -1,6 +1,7 @@
 package io.vertx.workshop.recommendation.impl;
 
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.EventBus;
@@ -24,15 +25,42 @@ public class RecommendationServiceImpl implements RecommendationService {
   @Override
   public void vote(String name, boolean plus, Handler<AsyncResult<Void>> handler) {
     redis.hincrby(name, plus ? "up" : "down", 1, hincrby -> {
-      // TODO to implement
-      // TODO don't forget to publish the new ratings to RECOMMENDATIONS_ADDRESS
+      // TO IMPLEMENT
+      if (hincrby.failed()) {
+        handler.handle(Future.failedFuture(hincrby.cause()));
+      } else {
+        redis.hgetall(name, hgetall -> {
+          if (hgetall.failed()) {
+            handler.handle(Future.failedFuture(hgetall.cause()));
+          } else {
+            JsonObject result = hgetall.result();
+            if (result == null) {
+              result = new JsonObject();
+            }
+
+            handler.handle(Future.succeededFuture());
+
+            // we broadcast the update so everyone can know about it
+            eventBus.publish("devoxx.recommendations.announce", result.put("name", name));
+          }
+        });
+      }
     });
   }
 
   @Override
   public void get(String name, Handler<AsyncResult<JsonObject>> handler) {
     redis.hgetall(name, hgetall -> {
-      // TODO to implement
+      // TO IMPLEMENT
+      if (hgetall.failed()) {
+        handler.handle(Future.failedFuture(hgetall.cause()));
+      } else {
+        JsonObject result = hgetall.result();
+        if (result == null) {
+          result = new JsonObject();
+        }
+        handler.handle(Future.succeededFuture(result.put("name", name)));
+      }
     });
   }
 
