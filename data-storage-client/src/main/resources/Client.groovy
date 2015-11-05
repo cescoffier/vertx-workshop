ebd625cceb238af00b160d77941e81be55491a69
Clement Escoffier <clement.escoffier@gmail.com>
2015-11-05 17:20:02 +0100

    Implement step 4

--- data-storage-client/src/main/resources/Client.groovy
+++ data-storage-client/src/main/resources/Client.groovy
@@ -4,10 +4,14 @@ import io.vertx.workshop.groovy.data.*
 def service = DataStorageService.createProxy(vertx, "devoxx.places")
 
 service.getAllPlaces({
-    /**
-     * TODO to implement
-     */
   result ->
+    if (result.failed()) {
+      println "Cannot retrieve the list of places : " + result.cause()
+    } else {
+      println "Done"
+      def list = result.result()
+      list.each { map -> println map["name"] }
+    }
 })
 
 
