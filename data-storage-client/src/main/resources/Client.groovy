c92973a1465c3b1a7e9c3a6913184f1a62f61e9d
Clement Escoffier <clement.escoffier@gmail.com>
2015-11-09 15:53:46 +0100

    yet another deployment

--- data-storage-client/src/main/resources/Client.groovy
+++ data-storage-client/src/main/resources/Client.groovy
@@ -8,6 +8,13 @@ service.getAllPlaces({
      * TODO to implement
      */
   result ->
+    if (result.failed()) {
+      println "Cannot retrieve the list of places : " + result.cause()
+    } else {
+      println "Done"
+      def list = result.result()
+      list.each { map -> println map["name"] }
+    }
 })
 
 
--- data-storage-service/src/main/java/io/vertx/workshop/data/impl/DataStorageServiceImpl.java
+++ data-storage-service/src/main/java/io/vertx/workshop/data/impl/DataStorageServiceImpl.java
@@ -5,9 +5,9 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
 import io.vertx.workshop.data.DataStorageService;
 import io.vertx.workshop.data.Place;
-import io.vertx.ext.mongo.MongoClient;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -32,9 +32,16 @@ public class DataStorageServiceImpl implements DataStorageService {
 
   @Override
   public void getAllPlaces(Handler<AsyncResult<List<Place>>> resultHandler) {
-    /**
-     * TODO to implement.
-     */
+    mongo.find(COLLECTION, new JsonObject(),
+        ar -> {
+          if (ar.failed()) {
+            resultHandler.handle(Future.failedFuture(ar.cause()));
+          } else {
+            List<Place> places = ar.result().stream()
+                .map(Place::new).collect(Collectors.toList());
+            resultHandler.handle(Future.succeededFuture(places));
+          }
+        });
   }
 
   public void getPlacesForCategory(String category,
@@ -56,9 +63,18 @@ public class DataStorageServiceImpl implements DataStorageService {
 
   @Override
   public void getPlacesForTag(String tag, Handler<AsyncResult<List<Place>>> resultHandler) {
-    /**
-     * TODO to implement.
-     */
+    mongo.find(COLLECTION,
+        new JsonObject().put("tags", tag),
+        ar -> {
+          if (ar.failed()) {
+            resultHandler.handle(Future.failedFuture(ar.cause()));
+          } else {
+            List<Place> places = ar.result().stream()
+                .map(Place::new).collect(Collectors.toList());
+            resultHandler.handle(Future.succeededFuture(places));
+          }
+        }
+    );
   }
 
   @Override
